d44ccd83af3a08f1e7f8eb38aae8212c95bad22e
Curtis Vogt <curtis.vogt@invenia.ca>
2015-07-31 09:41:18 -0500

    Added testcases for Olsen.parsedate.

--- test/timezones/Olsen.jl
+++ test/timezones/Olsen.jl
@@ -1,4 +1,5 @@
 import TimeZones.Olsen: Time, hour, minute, second, as_seconds, hourminutesecond
+import TimeZones.Olsen: parsedate
 import Base.Dates: Hour, Minute, Second
 
 # Time seconds constructor
@@ -45,3 +46,48 @@ t = Time(1,-23,-45)
 # Math
 @test Time(1,23,45) + Time(-1,-23,-45) == Time(0)
 @test Time(1,23,45) - Time(1,23,45) == Time(0)
+
+# Variations of until dates
+@test parsedate("1945") == (DateTime(1945), 0)
+@test parsedate("1945 Aug") == (DateTime(1945,8), 0)
+@test parsedate("1945 Aug 2") == (DateTime(1945,8,2), 0)
+@test parsedate("1945 Aug 2 12") == (DateTime(1945,8,2,12), 0)  # Doesn't actually occur
+@test parsedate("1945 Aug 2 12:34") == (DateTime(1945,8,2,12,34), 0)
+@test parsedate("1945 Aug 2 12:34:56") == (DateTime(1945,8,2,12,34,56), 0)
+
+# Make sure parsing can handle additional spaces.
+@test parsedate("1945  Aug") == (DateTime(1945,8), 0)
+@test parsedate("1945  Aug  2") == (DateTime(1945,8,2), 0)
+@test parsedate("1945  Aug  2  12") == (DateTime(1945,8,2,12), 0)
+@test parsedate("1945  Aug  2  12:34") == (DateTime(1945,8,2,12,34), 0)
+@test parsedate("1945  Aug  2  12:34:56") == (DateTime(1945,8,2,12,34,56), 0)
+
+# Explicit zone "local wall time"
+@test_throws Exception parsedate("1945w")
+@test_throws Exception parsedate("1945 Augw")
+@test_throws Exception parsedate("1945 Aug 2w")
+@test parsedate("1945 Aug 2 12w") == (DateTime(1945,8,2,12), 0)
+@test parsedate("1945 Aug 2 12:34w") == (DateTime(1945,8,2,12,34), 0)
+@test parsedate("1945 Aug 2 12:34:56w") == (DateTime(1945,8,2,12,34,56), 0)
+
+# Explicit zone "UTC time"
+@test_throws Exception parsedate("1945u")
+@test_throws Exception parsedate("1945 Augu")
+@test_throws Exception parsedate("1945 Aug 2u")
+@test parsedate("1945 Aug 2 12u") == (DateTime(1945,8,2,12), 1)
+@test parsedate("1945 Aug 2 12:34u") == (DateTime(1945,8,2,12,34), 1)
+@test parsedate("1945 Aug 2 12:34:56u") == (DateTime(1945,8,2,12,34,56), 1)
+
+# Explicit zone "standard time"
+@test_throws Exception parsedate("1945s")
+@test_throws Exception parsedate("1945 Augs")
+@test_throws Exception parsedate("1945 Aug 2s")
+@test parsedate("1945 Aug 2 12s") == (DateTime(1945,8,2,12), 2)
+@test parsedate("1945 Aug 2 12:34s") == (DateTime(1945,8,2,12,34), 2)
+@test parsedate("1945 Aug 2 12:34:56s") == (DateTime(1945,8,2,12,34,56), 2)
+
+# Invalid zone
+@test_throws Exception parsedate("1945 Aug 2 12:34i")
+
+# Actual until date found in Zone "Pacific/Apia"
+@test parsedate("2011 Dec 29 24:00") == (DateTime(2011,12,30), 0)
