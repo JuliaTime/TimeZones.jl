77cf9aa8f1ac80f4ff4e280a5ec231326c66ee52
Curtis Vogt <curtis.vogt@invenia.ca>
2015-08-05 14:49:02 -0500

    Refactoring build.jl to be more user friendly.

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,4 @@
+import TimeZones.Olsen: REGIONS, generate_tzdata
 
 dir = dirname(@__FILE__)
 tz = joinpath(dir, "tzdata")
@@ -13,19 +14,18 @@ for d in (tz, com)
     end
 end
 
-regions = ["africa","antarctica","asia","australasia",
-           "europe","northamerica","southamerica"]
-
 #=
 Need to make this code more reliable
 
-LoadError: failed process: Process(`curl -o /Users/omus/.julia/v0.4/Timezones/deps/tzdata/asia -L ftp://ftp.iana.org/tz/data/asia`, ProcessExited(56)) [56]
-while loading /Users/omus/.julia/v0.4/Timezones/deps/build.jl, in expression starting on line 18
+LoadError: failed process: Process(`curl -o ~/.julia/v0.4/Timezones/deps/tzdata/asia -L ftp://ftp.iana.org/tz/data/asia`, ProcessExited(56)) [56]
+while loading ~/.julia/v0.4/Timezones/deps/build.jl, in expression starting on line 18
 =#
-@sync for reg in regions
-    @async download("ftp://ftp.iana.org/tz/data/"*reg,joinpath(tz,reg))
+info("Downloading TZ data")
+@sync for region in REGIONS
+    @async download("ftp://ftp.iana.org/tz/data/"*region, joinpath(tz,region))
 end
 
-include("../src/TimeZones.jl")
-TimeZones.Olsen.generate_tzdata(tz,com)
-println("=========Timezone Database Successfully Compiled=========")
+info("Pre-processing TimeZone data")
+generate_tzdata(tz,com)
+
+info("Successfully processed TimeZone data")
