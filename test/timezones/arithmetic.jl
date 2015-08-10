ae4b876cc80c5870f06c0fa79c3c372ac22b4600
Curtis Vogt <curtis.vogt@invenia.ca>
2015-08-10 11:34:42 -0500

    Added testcases that showcase ZonedDatetime associativity.

--- test/timezones/arithmetic.jl
+++ test/timezones/arithmetic.jl
@@ -19,3 +19,9 @@ fall = DateTime(2015, 10, 25, 0)   # 25 hour day in warsaw
 @test ZonedDateTime(normal, warsaw) + Hour(24) == ZonedDateTime(normal + Hour(24), warsaw)
 @test ZonedDateTime(spring, warsaw) + Hour(24) == ZonedDateTime(spring + Hour(25), warsaw)
 @test ZonedDateTime(fall, warsaw) + Hour(24) == ZonedDateTime(fall + Hour(23), warsaw)
+
+# Non-Associativity
+hour_day = (ZonedDateTime(spring, warsaw) + Hour(24)) + Day(1)
+day_hour = (ZonedDateTime(spring, warsaw) + Day(1)) + Hour(24)
+
+@test hour_day - day_hour == Hour(1)
