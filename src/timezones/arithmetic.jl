87a4798d9c36319350625cf5bde99f95f84d376b
Curtis Vogt <curtis.vogt@invenia.ca>
2015-08-20 13:59:52 -0500

    Made use of abstract types DatePeriod and TimePeriod.

--- src/timezones/arithmetic.jl
+++ src/timezones/arithmetic.jl
@@ -1,19 +1,16 @@
-typealias LocalPeriod Union{Year,Month,Week,Day}
-typealias UTCPeriod Union{Hour,Minute,Second,Millisecond}
-
 # ZonedDateTime arithmetic
 (+)(x::ZonedDateTime) = x
 (-)(x::ZonedDateTime,y::ZonedDateTime) = x.utc_datetime - y.utc_datetime
 
-function (+)(dt::ZonedDateTime,p::LocalPeriod)
+function (+)(dt::ZonedDateTime,p::DatePeriod)
     return ZonedDateTime(localtime(dt) + p, dt.timezone)
 end
-function (+)(dt::ZonedDateTime,p::UTCPeriod)
+function (+)(dt::ZonedDateTime,p::TimePeriod)
     return ZonedDateTime(dt.utc_datetime + p, dt.timezone; from_utc=true)
 end
-function (-)(dt::ZonedDateTime,p::LocalPeriod)
+function (-)(dt::ZonedDateTime,p::DatePeriod)
     return ZonedDateTime(localtime(dt) - p, dt.timezone)
 end
-function (-)(dt::ZonedDateTime,p::UTCPeriod)
+function (-)(dt::ZonedDateTime,p::TimePeriod)
     return ZonedDateTime(dt.utc_datetime - p, dt.timezone; from_utc=true)
 end
