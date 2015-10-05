cbce96debed0d0101d656295c7a866fcb30b79e3
Curtis Vogt <curtis.vogt@gmail.com>
2015-10-05 13:14:40 -0500

    Refactored localzone code

--- src/timezones/local.jl
+++ src/timezones/local.jl
@@ -2,111 +2,126 @@
 # Based upon Python's tzlocal https://pypi.python.org/pypi/tzlocal
 
 @osx_only function localzone()
-    zone = readall(`systemsetup -gettimezone`)
-    if contains(zone, "Time Zone: ")
-        zone = strip(replace(zone, "Time Zone: ", ""))
+    name = readall(`systemsetup -gettimezone`)
+    if contains(name, "Time Zone: ")
+        name = strip(replace(name, "Time Zone: ", ""))
     else
-        zone = readlink("/etc/localtime")
-        # link will be something like /usr/share/zoneinfo/America/Winnipeg
-        zone = match(r"(?<=zoneinfo/).*$", zone).match
+        # link will be something like /usr/share/zoneinfo/Europe/Warsaw
+        name = readlink("/etc/localtime")
+        name = match(r"(?<=zoneinfo/).*$", name).match
     end
-    return zone
+    return TimeZone(name)
 end
 
 @linux_only function localzone()
+    name = ""
     validnames = timezone_names()
 
-    # Try getting the time zone "TZ" environment variable
+    # Try getting the time zone from the "TZ" environment variable
     # http://linux.die.net/man/3/tzset
-    zone = Nullable{AbstractString}(get(ENV, "TZ", nothing))
-    if !isnull(zone)
-        zone_str = get(zone)
-        if startswith(zone_str,':')
-            zone_str = zone_str[2:end]
+    if haskey(ENV, "TZ")
+        name = ENV["TZ"]
+        startswith(name, ':') || error("Currently only support filespec for TZ variable")
+        name = name[2:end]
+
+        if startswith(name, '/')
+            return open(name) do f
+                read_tzfile(f, "local")
+            end
+        else
+            # Relative name matches pre-compiled timezone name
+            name in validnames && return TimeZone(name)
+
+            # The system timezone directory used depends on the (g)libc version
+            tzdirs = ["/usr/lib/zoneinfo", "/usr/share/zoneinfo"]
+            haskey(ENV, "TZDIR") && unshift!(tzdirs, ENV["TZDIR"])
+
+            for dir in tzdirs
+                filepath = joinpath(dir, name)
+                isfile(filepath) || continue
+                return open(filepath) do f
+                    read_tzfile(f, name)
+                end
+            end
+
+            throw(SystemError("unable to locate tzfile: $name"))
         end
-        zone_str in validnames && return zone_str
-
-        # TODO Read the tzfile to get the timezone
-
-        error("Failed to resolve local timezone from \"TZ\" environment variable. ",
-            "Only currently support timezone names as the \"TZ\" environment variable")
     end
 
-    # Look for distribution specific configuration files
-    # that contain the timezone name.
+    # Look for distribution specific configuration files that contain the timezone name.
 
     filename = "/etc/timezone"
     if isfile(filename)
-        zone = ""
         open(filename) do file
-            zone = readall(file)
+            name = readall(file)
+
             # Get rid of host definitions and comments:
-            zone = strip(replace(zone, r"#.*", ""))
-            zone = replace(zone, ' ', '_')
+            name = strip(replace(name, r"#.*", ""))
+            name = replace(name, ' ', '_')
         end
-        zone in validnames && return zone
+
+        name in validnames && return TimeZone(name)
     end
 
     # CentOS has a ZONE setting in /etc/sysconfig/clock,
     # OpenSUSE has a TIMEZONE setting in /etc/sysconfig/clock and
     # Gentoo has a TIMEZONE setting in /etc/conf.d/clock
 
-    zone_re = r"(?:TIME)?ZONE\s*=\s*\"(.*?)\""
-    for filename in ("/etc/sysconfig/clock", "/etc/conf.d/clock")
-        isfile(filename) || continue
-        file = open(filename)
-        try # Make sure we close the file
+    zone_re = r"(TIME)?ZONE\s*=\s*\"(?<name>.*?)\""
+    for filepath in ("/etc/sysconfig/clock", "/etc/conf.d/clock")
+        isfile(filepath) || continue
+        open(filepath) do file
             for line in readlines(file)
                 matched = match(zone_re, line)
                 if matched != nothing
-                    zone = matched.captures[1]
-                    zone = replace(zone, ' ', '_')
-
-                    zone in validnames && return zone
+                    name = matched.captures["name"]
+                    name = replace(name, ' ', '_')
+                    break
                 end
             end
-        finally
-            close(file)
         end
+
+        name in validnames && return TimeZone(name)
     end
 
     # systemd distributions use symlinks that include the zone name,
     # see manpage of localtime(5) and timedatectl(1)
     link = "/etc/localtime"
     if islink(link)
-        zone = readlink(link)
-        start = search(zone, '/')
+        filepath = readlink(link)
+        start = search(filepath, '/')
 
         while start != 0
-            zone = zone[(start+1):end]
-
-            zone in validnames && return zone
-
-            start = search(zone, '/')
+            name = filepath[(start + 1):end]
+            name in validnames && return TimeZone(name)
+            start = search(filepath, '/', start + 1)
         end
     end
 
-    # TODO No explicit setting existed. Use localtime
+    # No explicit setting existed. Use localtime
+    for filepath in ("/etc/localtime", "/usr/local/etc/localtime")
+        isfile(filepath) || continue
+        return open(filepath) do f
+            read_tzfile(f, "local")
+        end
+    end
 
     error("Failed to find local timezone")
 end
 
 @windows_only function localzone()
-    isfile(TRANSLATION_FILE) ||
-        error("Windows zones not found. Try running Pkg.build(\"TimeZones\")")
+    isfile(TRANSLATION_FILE) || error("Missing Windows to POSIX timezone translation ",
+        "file. Try running Pkg.build(\"TimeZones\")")
 
-    win_tz_dict = nothing
-    open(TRANSLATION_FILE, "r") do fp
-        win_tz_dict = deserialize(fp)
+    translation = open(TRANSLATION_FILE, "r") do fp
+        deserialize(fp)
     end
 
+    # Windows powershell should be available on Windows 7 and above
     winzone = strip(readall(`powershell -Command "[TimeZoneInfo]::Local.Id"`))
-    if haskey(win_tz_dict, winzone)
-        timezone = win_tz_dict[winzone]
+    if haskey(translation, winzone)
+        return TimeZone(translation[winzone])
     else
-        error("Failed to determine your Windows timezone. ",
-            "Uses powershell, should work on windows 7 and above")
+        error("unable to translate to POSIX timezone name: $winzone")
     end
-
-    return timezone
 end
