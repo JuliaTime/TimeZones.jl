a0646ce7338f95b6a8e6fff929ac996b6c5eeae2
Curtis Vogt <curtis.vogt@gmail.com>
2015-10-05 11:45:38 -0500

    Refactored implementation of loading a tzfile

--- src/timezones/tzfile.jl
+++ src/timezones/tzfile.jl
@@ -1,58 +1,50 @@
 # Parsing tzfiles (http://man7.org/linux/man-pages/man5/tzfile.5.html)
-type TTInfo
-    tt_gmtoff::Int32
-    tt_isdst::Int8
-    tt_abbrind::UInt8
+immutable TransitionTimeInfo
+    gmtoff::Int32     # tt_gmtoff
+    isdst::Int8       # tt_isdst
+    abbrindex::UInt8  # tt_abbrind
 end
 
-function tzfile(file::IO, zone::AbstractString)
-    magic = readbytes(file, 4)
+abbreviation(chars::Array{UInt8}, offset::Integer=1) = ascii(pointer(chars[offset:end]))
+
+function read_tzfile(io::IO, name::AbstractString)
+    magic = readbytes(io, 4)
     @assert magic == b"TZif" "Magic file identifier \"TZif\" not found."
-    version = readbytes(file, 1)
-    # Fifteen bytes containing zeros reserved for future use.
-    readbytes(file, 15)
-    # The number of UTC/local indicators stored in the file.
-    tzh_ttisgmtcnt = bswap(read(file, Int32))
-    # The number of standard/wall indicators stored in the file.
-    tzh_ttisstdcnt = bswap(read(file, Int32))
-    # The number of leap seconds for which data is stored in the file.
-    tzh_leapcnt = bswap(read(file, Int32))
-    # The number of "transition times" for which data is stored in the file.
-    tzh_timecnt = bswap(read(file, Int32))
-    # The number of "local time types" for which data is stored in the file (must not be zero).
-    tzh_typecnt = bswap(read(file, Int32))
-    # The number of characters of "timezone abbreviation strings" stored in the file.
-    tzh_charcnt = bswap(read(file, Int32))
+
+    version = readbytes(io, 1)  # Format version (ASCII NUL ('\0') or a '2' (0x32))
+    readbytes(io, 15)  # Fifteen bytes reserved for future use
+    tzh_ttisgmtcnt = bswap(read(io, Int32))  # Number of UTC/local indicators
+    tzh_ttisstdcnt = bswap(read(io, Int32))  # Number of standard/wall indicators
+    tzh_leapcnt = bswap(read(io, Int32))  # Number of leap seconds
+    tzh_timecnt = bswap(read(io, Int32))  # Number of transition dates
+    tzh_typecnt = bswap(read(io, Int32))  # Number of TransitionTimeInfos (must be > 0)
+    tzh_charcnt = bswap(read(io, Int32))  # Number of timezone abbreviation characters
 
     transitions = Array{Int32}(tzh_timecnt)
-    for index in 1:tzh_timecnt
-        transitions[index] = bswap(read(file, Int32))
+    for i in eachindex(transitions)
+        transitions[i] = bswap(read(io, Int32))
     end
     lindexes = Array{UInt8}(tzh_timecnt)
-    for index in 1:tzh_timecnt
-        lindexes[index] = bswap(read(file, UInt8)) + 1 # Julia uses 1 indexing
+    for i in eachindex(lindexes)
+        lindexes[i] = bswap(read(io, UInt8)) + 1 # Julia uses 1 indexing
     end
-    ttinfo = Array{TTInfo}(tzh_typecnt)
-    for index in 1:tzh_typecnt
-        ttinfo[index] = TTInfo(
-            bswap(read(file, Int32)),
-            bswap(read(file, Int8)),
-            bswap(read(file, UInt8)) + 1 # Julia uses 1 indexing
+    ttinfo = Array{TransitionTimeInfo}(tzh_typecnt)
+    for i in eachindex(ttinfo)
+        ttinfo[i] = TransitionTimeInfo(
+            bswap(read(io, Int32)),
+            bswap(read(io, Int8)),
+            bswap(read(io, UInt8)) + 1 # Julia uses 1 indexing
         )
     end
-    tznames_raw = Array{UInt8}(tzh_charcnt)
-    namestart = 1
-    tznames = Dict{UInt8, String}()
-    for index in 1:tzh_charcnt
-        tznames_raw[index] = bswap(read(file, UInt8))
-        if tznames_raw[index] == '\0'
-            tznames[namestart] = ascii(tznames_raw[namestart:index-1])
-            namestart = index+1
-        end
+    abbrs = Array{UInt8}(tzh_charcnt)
+    for i in eachindex(abbrs)
+        abbrs[i] = bswap(read(io, UInt8))
     end
+
     # Now build the timezone object
     if length(transitions) == 0
-        return FixedTimeZone(Symbol(tznames[ttinfo[1].tt_abbrind]), Offset(ttinfo[1].tt_gmtoff))
+        abbr = abbreviation(abbrs, ttinfo[1].abbrindex)
+        return FixedTimeZone(Symbol(abbr), Offset(ttinfo[1].gmtoff))
     else
         # Calculate transition info
         transition_info = Transition[]
@@ -60,47 +52,36 @@ function tzfile(file::IO, zone::AbstractString)
         prev_dst = 0
         dst = 0
         utc = 0
-        for i in 1:length(transitions)
+        for i in eachindex(transitions)
             inf = ttinfo[lindexes[i]]
-            utcoffset = inf.tt_gmtoff
-            if inf.tt_isdst == 0
-                utc = inf.tt_gmtoff
+            utcoffset = inf.gmtoff
+            if inf.isdst == 0
+                utc = inf.gmtoff
                 dst = 0
             else
                 if prev_dst == 0
                     utc = prev_utc
-                    dst = inf.tt_gmtoff - prev_utc
+                    dst = inf.gmtoff - prev_utc
                 else
-                    utc = inf.tt_gmtoff - prev_dst
+                    utc = inf.gmtoff - prev_dst
                     dst = prev_dst
                 end
             end
-            if haskey(tznames, inf.tt_abbrind)
-                tzname = tznames[inf.tt_abbrind]
-            else
-                # Sometimes it likes to be fancy and have multiple names in one for
-                # example "WSST" at tt_abbrind 5 turns into "SST" at tt_abbrind 6
-                name_offset = 1
-                while !haskey(tznames, (inf.tt_abbrind-name_offset))
-                    name_offset+=1
-                    if name_offset >= inf.tt_abbrind
-                        error("Failed to find a tzname referenced in a transition.")
-                    end
-                end
-                tzname = tznames[inf.tt_abbrind-name_offset][name_offset+1:end]
-                tznames[inf.tt_abbrind] = tzname
-            end
+
+            # Sometimes it likes to be fancy and have multiple names in one for
+            # example "WSST" at abbrindex 5 turns into "SST" at abbrindex 6
+            abbr = abbreviation(abbrs, inf.abbrindex)
             push!(
                 transition_info,
                 Transition(
                     unix2datetime(transitions[i]),
-                    FixedTimeZone(Symbol(tzname),
+                    FixedTimeZone(Symbol(abbr),
                     Offset(utc, dst))
                 )
             )
             prev_utc = utc
             prev_dst = dst
         end
-        return VariableTimeZone(Symbol(zone), transition_info)
+        return VariableTimeZone(Symbol(name), transition_info)
     end
 end
