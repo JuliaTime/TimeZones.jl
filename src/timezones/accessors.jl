f213857995117197908c76b03141594d88720d9e
Curtis Vogt <curtis.vogt@gmail.com>
2016-05-17 23:17:46 -0500

    Add documentation for localtime and utc

--- src/timezones/accessors.jl
+++ src/timezones/accessors.jl
@@ -1,4 +1,17 @@
+"""
+    localtime(::ZonedDateTime) -> DateTime
+
+Creates a local or civil `DateTime` from the given `ZonedDateTime`. For example the
+`2014-05-30T08:11:24-04:00` would return `2014-05-30T08:11:24`.
+"""
 localtime(zdt::ZonedDateTime) = zdt.utc_datetime + zdt.zone.offset
+
+"""
+    utc(::ZonedDateTime) -> DateTime
+
+Creates a utc `DateTime` from the given `ZonedDateTime`. For example the
+`2014-05-30T08:11:24-04:00` would return `2014-05-30T12:11:24`.
+"""
 utc(zdt::ZonedDateTime) = zdt.utc_datetime
 
 days(zdt::ZonedDateTime) = days(localtime(zdt))
