d2c062ab00b65ddce51cba748882efe81508f868
Curtis Vogt <curtis.vogt@gmail.com>
2016-08-12 11:09:46 -0500

    Cleanup adjusters.jl

--- src/timezones/adjusters.jl
+++ src/timezones/adjusters.jl
@@ -1,15 +1,16 @@
-import Base.Dates: Year, Month, Day, Hour, Minute, Second, Millisecond
+import Base.Dates: trunc, DatePeriod, TimePeriod
 import Base.Dates: firstdayofweek, lastdayofweek, firstdayofmonth, lastdayofmonth,
     firstdayofyear, lastdayofyear, firstdayofquarter, lastdayofquarter
 
+
 # Truncation
-function Base.trunc{P<:DatePeriod}(zdt::ZonedDateTime, ::Type{P})
+function trunc{P<:DatePeriod}(zdt::ZonedDateTime, ::Type{P})
     ZonedDateTime(trunc(localtime(zdt), P), timezone(zdt))
 end
-function Base.trunc{P<:TimePeriod}(zdt::ZonedDateTime, ::Type{P})
+function trunc{P<:TimePeriod}(zdt::ZonedDateTime, ::Type{P})
     ZonedDateTime(trunc(utc(zdt), P), timezone(zdt), from_utc=true)
 end
-Base.trunc(zdt::ZonedDateTime, ::Type{Millisecond}) = zdt
+trunc(zdt::ZonedDateTime, ::Type{Millisecond}) = zdt
 
 # Adjusters
 for prefix in ("firstdayof", "lastdayof"), suffix in ("week", "month", "year", "quarter")
