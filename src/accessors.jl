46bf9db7bd71b6a2c71db3e0aa1482c21832e0da
Curtis Vogt <curtis.vogt@gmail.com>
2016-09-12 09:11:42 -0500

    Removed use of `@vectorize_1arg` for 0.6
    See https://github.com/JuliaLang/julia/pull/17302

--- src/accessors.jl
+++ src/accessors.jl
@@ -27,8 +27,6 @@ for period in (:Hour, :Minute, :Second, :Millisecond)
     accessor = Symbol(lowercase(string(period)))
     @eval begin
         $accessor(zdt::ZonedDateTime) = $accessor(localtime(zdt))
-        @vectorize_1arg ZonedDateTime $accessor
-
         $period(zdt::ZonedDateTime) = $period($accessor(zdt))
     end
 end
