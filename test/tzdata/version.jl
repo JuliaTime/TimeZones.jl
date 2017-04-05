ee5927050ed0954c0b502a1639d02403b855b6ee
Curtis Vogt <curtis.vogt@gmail.com>
2017-04-05 17:19:55 -0500

    Move tzdata tests into folder

--- test/tzdata/version.jl
+++ test/tzdata/version.jl
@@ -1,5 +1,33 @@
 import TimeZones: ARCHIVE_DIR
+import TimeZones.TZData: TZDATA_VERSION_REGEX, TZDATA_NEWS_REGEX
 import TimeZones.TZData: read_news, extract, tzdata_version_dir, tzdata_version_archive
+
+for year = ("12", "1234"), letter = ("", "z")
+    version = year * letter
+    @test match(TZDATA_VERSION_REGEX, version).match == version
+    @test match(TZDATA_VERSION_REGEX, "tzdata$version.tar.gz").match == version
+end
+
+for year = ("1", "123", "12345"), letter = ("", "a")
+    version = year * letter
+    @test match(TZDATA_VERSION_REGEX, version) === nothing
+    @test match(TZDATA_VERSION_REGEX, "tzdata$version.tar.gz") === nothing
+end
+
+@test match(TZDATA_NEWS_REGEX, "Release 92")[:version] == "92"
+@test match(TZDATA_NEWS_REGEX, "Release 92c")[:version] == "92c"
+@test match(TZDATA_NEWS_REGEX, "Release data94a")[:version] == "94a"
+@test match(TZDATA_NEWS_REGEX, "Release code94c") === nothing
+@test match(TZDATA_NEWS_REGEX, "Release code95f-data95j")[:version] == "95j"
+@test match(TZDATA_NEWS_REGEX, "Release data1996m")[:version] == "1996m"
+@test match(TZDATA_NEWS_REGEX, "Release code1996n") === nothing
+@test match(TZDATA_NEWS_REGEX, "Release code1996o-data1996n")[:version] == "1996n"
+@test match(TZDATA_NEWS_REGEX, "Release 1997a")[:version] == "1997a"
+
+@test match(TZDATA_NEWS_REGEX, "Release 1") === nothing
+@test match(TZDATA_NEWS_REGEX, "Release 199") === nothing
+@test match(TZDATA_NEWS_REGEX, "Release 19999") === nothing
+
 
 archive = joinpath(ARCHIVE_DIR, "tzdata$TZDATA_VERSION.tar.gz")
 
