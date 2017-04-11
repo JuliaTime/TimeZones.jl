49d77da5ff25b6973ed406ccc65cf7a85de14bf8
Curtis Vogt <curtis.vogt@gmail.com>
2017-04-11 13:33:53 -0500

    Small note to clarify terminology

--- src/tzdata/TZData.jl
+++ src/tzdata/TZData.jl
@@ -1,5 +1,9 @@
 module TZData
 
+# Note: The tz database is made up of two parts: code and data. TimeZones.jl only requires
+# the "tzdata" archive or more specifically the "tz source" files within the archive
+# (africa, australasia, ...)
+
 export build
 
 include("timeoffset.jl")
