f81ccca65b7db20331f6c5d682323dacbb341b89
Curtis Vogt <curtis.vogt@gmail.com>
2017-05-17 15:02:23 -0500

    Add test for UnhandledTimeError showerror

--- test/types.jl
+++ test/types.jl
@@ -35,12 +35,18 @@ import Base.Dates: Hour, Second, UTM
 
 
 # Test exception messages
-tz = FixedTimeZone("Imaginary/Zone", 0, 0)
+tz = VariableTimeZone(
+    "Imaginary/Zone",
+    [Transition(DateTime(1800,1,1), FixedTimeZone("IST",0,0))],
+    DateTime(1980,1,1),
+)
 
 @test sprint(showerror, AmbiguousTimeError(DateTime(2015,1,1), tz)) ==
     "AmbiguousTimeError: Local DateTime 2015-01-01T00:00:00 is ambiguous within Imaginary/Zone"
 @test sprint(showerror, NonExistentTimeError(DateTime(2015,1,1), tz)) ==
     "NonExistentTimeError: Local DateTime 2015-01-01T00:00:00 does not exist within Imaginary/Zone"
+@test sprint(showerror, UnhandledTimeError(tz)) ==
+    "UnhandledTimeError: TimeZone Imaginary/Zone does not handle dates on or after 1980-01-01T00:00:00 UTC"
 
 
 warsaw = resolve("Europe/Warsaw", tzdata["europe"]...)
