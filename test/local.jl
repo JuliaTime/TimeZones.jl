4c7e00113315a461dc9b71409ae890ec23faa559
Curtis Vogt <curtis.vogt@gmail.com>
2017-09-03 11:46:43 -0500

    Support first TZ format
    When extracting time zone information from the TZ environmental variable
    we previously only supported loading information from a file. We now
    also support the `std offset` specification which always produces a
    FixedTimeZone.
    
    The introduction of this change was required by a change to the
    Travis CI environment where the time zone is now specified as `TZ="UTC"`

--- test/local.jl
+++ test/local.jl
@@ -1,5 +1,38 @@
-import TimeZones: TimeZone, localzone
+import TimeZones: TimeZone, localzone, parse_tz_format
 import Compat: is_linux
+
+# Parse the TZ environment variable format
+# Should mirror the behaviour of running:
+# `date -u; env TZ="..." date`
+
+@test_throws ArgumentError parse_tz_format("AB")
+@test_throws ArgumentError parse_tz_format("+")
+@test_throws ArgumentError parse_tz_format("12")
+
+@test parse_tz_format("") == FixedTimeZone("UTC", 0, 0)
+@test parse_tz_format("ABC") == FixedTimeZone("ABC", 0, 0)
+@test parse_tz_format("ABC+") == FixedTimeZone("ABC", 0, 0)
+@test parse_tz_format("ABC-") == FixedTimeZone("ABC", 0, 0)
+
+@test parse_tz_format("ABC1") == FixedTimeZone("ABC", -1 * 3600, 0)
+@test parse_tz_format("ABC+1") == FixedTimeZone("ABC", -1 * 3600, 0)
+@test parse_tz_format("ABC-1") == FixedTimeZone("ABC", 1 * 3600, 0)
+
+@test parse_tz_format("ABC-24")  == FixedTimeZone("ABC", 24 * 3600, 0)
+@test parse_tz_format("ABC-25")  == FixedTimeZone("ABC", 24 * 3600, 0)
+@test parse_tz_format("ABC-100") == FixedTimeZone("ABC", 24 * 3600, 0)
+
+@test parse_tz_format("ABC-00:59")  == FixedTimeZone("ABC", 59 * 60, 0)
+@test parse_tz_format("ABC-00:99")  == FixedTimeZone("ABC", 59 * 60, 0)
+@test parse_tz_format("ABC-00:100") == FixedTimeZone("ABC", 59 * 60, 0)
+
+@test parse_tz_format("ABC-00:00:59")  == FixedTimeZone("ABC", 59, 0)
+@test parse_tz_format("ABC-00:00:99")  == FixedTimeZone("ABC", 59, 0)
+@test parse_tz_format("ABC-00:00:100") == FixedTimeZone("ABC", 59, 0)
+
+@test_throws ArgumentError parse_tz_format("ABC+00:")  # Terminal test is valid and equal to "ABC"
+@test_throws ArgumentError parse_tz_format("ABC+12:")
+
 
 # Ensure that the current system's local time zone is supported. If this test fails make
 # sure to report it as an issue.
@@ -7,12 +40,25 @@ import Compat: is_linux
 
 
 if is_linux()
-    # Bad TZ environmental variables
-    withenv("TZ" => "") do
-        @test_throws ErrorException localzone()
+    # Bad TZ environment variable formats
+    withenv("TZ" => "+12:00") do
+        @test_throws ArgumentError localzone()
     end
     withenv("TZ" => "Europe/Warsaw") do
-        @test_throws ErrorException localzone()
+        @test_throws ArgumentError localzone()
+    end
+
+    # Currently unsupported TZ environment variable formats
+    withenv("TZ" => "NZST-12:00:00NZDT-13:00:00,M10.1.0,M3.3.0") do
+        @test_throws ArgumentError localzone()
+    end
+
+    # Valid TZ formats
+    withenv("TZ" => "") do
+        @test localzone() == utc
+    end
+    withenv("TZ" => "UTC") do
+        @test localzone() == utc
     end
 
     # Absolute filespec
