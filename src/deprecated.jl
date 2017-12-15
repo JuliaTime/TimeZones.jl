2895d6cfb025c385682fb9ef964525a6b392216e
Curtis Vogt <curtis.vogt@gmail.com>
2017-12-15 13:51:54 -0600

    Set ZonedDateTime range default step on Julia 0.6

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,4 +1,4 @@
-import Base: @deprecate_binding, @deprecate
+import Base: @deprecate, @deprecate_binding, colon
 
 # BEGIN TimeZones 0.4 deprecations
 
@@ -12,3 +12,12 @@ import TimeZones.TZData: REGIONS, LEGACY_REGIONS
 
 # BEGIN TimeZones 0.5 deprecations
 # END TimeZones 0.5 deprecations
+
+# JuliaLang/julia#24258
+if VERSION < v"0.7.0-DEV.2778"
+    # Only remove this method when support for Julia 0.6 is dropped.
+    colon(start::T, stop::T) where {T<:ZonedDateTime} = start:Day(1):stop
+else
+    # Only remove this deprecation when support for Julia 0.7 is dropped.
+    @deprecate colon(start::T, stop::T) where {T<:ZonedDateTime}   start:Day(1):stop   false
+end
