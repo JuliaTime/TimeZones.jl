b3d57374991ecd3ffa9f9fa744a260c9fd009887
Curtis Vogt <curtis.vogt@gmail.com>
2018-01-16 13:08:49 -0600

    Prepare deprecations for TimeZones.jl 0.6

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,17 +1,6 @@
 import Base: @deprecate, @deprecate_binding, colon
 
-# BEGIN TimeZones 0.4 deprecations
-
-import TimeZones.TZData: REGIONS, LEGACY_REGIONS
-
-# We forgot to remove these deprecates during the switch to 0.5
-@deprecate_binding Olson TZData
-@deprecate_binding TZDATA_DIR TZ_SOURCE_DIR
-
-# END TimeZones 0.4 deprecations
-
-# BEGIN TimeZones 0.5 deprecations
-# END TimeZones 0.5 deprecations
+# BEGIN TimeZones 0.6 deprecations
 
 # JuliaLang/julia#24258
 if VERSION < v"0.7.0-DEV.2778"
@@ -21,3 +10,5 @@ else
     # Only remove this deprecation when support for Julia 0.7 is dropped.
     @deprecate colon(start::T, stop::T) where {T<:ZonedDateTime}   start:Day(1):stop   false
 end
+
+# END TimeZones 0.6 deprecations
