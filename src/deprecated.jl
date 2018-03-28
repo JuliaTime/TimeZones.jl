a909ff7dbf16b973e1260bba14d864e80cf5aa07
Curtis Vogt <curtis.vogt@gmail.com>
2018-03-28 15:40:35 -0500

    Address colon deprecation

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,14 +1,16 @@
-import Base: @deprecate, @deprecate_binding, colon
+import Base: @deprecate, @deprecate_binding, colon, (:)
 
 # BEGIN TimeZones 0.6 deprecations
 
 # JuliaLang/julia#24258
 if VERSION < v"0.7.0-DEV.2778"
     # Only remove this method when support for Julia 0.6 is dropped.
-    colon(start::T, stop::T) where {T<:ZonedDateTime} = start:Day(1):stop
+    colon(start::T, stop::T) where {T <: ZonedDateTime} = start:Day(1):stop
+elseif VERSION < v"0.7.0-DEV.4003"  # JuliaLang/julia#26074
+    @deprecate colon(start::T, stop::T) where {T <: ZonedDateTime} start:Day(1):stop  false
 else
     # Only remove this deprecation when support for Julia 0.7 is dropped.
-    @deprecate colon(start::T, stop::T) where {T<:ZonedDateTime}   start:Day(1):stop   false
+    @deprecate (:)(start::T, stop::T) where {T <: ZonedDateTime}   start:Day(1):stop  false
 end
 
 # END TimeZones 0.6 deprecations
