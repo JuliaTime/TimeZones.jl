e4084c75fd7e96d3d064bbcb7143dd2f3c8388b1
Curtis Vogt <curtis.vogt@gmail.com>
2018-03-28 17:06:58 -0500

    Handle exceptions in ignore_output

--- test/helpers.jl
+++ test/helpers.jl
@@ -12,17 +12,19 @@ function ignore_output(body::Function; stdout::Bool=true, stderr::Bool=true)
         (err_rd, err_wr) = redirect_stderr()
     end
 
-    result = body()
-
-    if stdout
-        redirect_stdout(out_old)
-        close(out_wr)
-        close(out_rd)
-    end
-    if stderr
-        redirect_stderr(err_old)
-        close(err_wr)
-        close(err_rd)
+    result = try
+        body()
+    finally
+        if stdout
+            redirect_stdout(out_old)
+            close(out_wr)
+            close(out_rd)
+        end
+        if stderr
+            redirect_stderr(err_old)
+            close(err_wr)
+            close(err_rd)
+        end
     end
 
     return result
