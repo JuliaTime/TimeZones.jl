cb0d8767fc24bf4a41fe0b25759e6876fd1d2ec4
Curtis Vogt <curtis.vogt@gmail.com>
2019-09-09 09:45:29 -0500

    Drop support for Julia 0.7

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,14 +1,4 @@
 using Base: @deprecate, @deprecate_binding
-import Base: (:)
-
-# BEGIN TimeZones 0.6 deprecations
-
-# Only remove this deprecation when support for Julia 0.7 is dropped (JuliaLang/julia#24258)
-if VERSION < v"1.0.0-DEV.44"
-    @deprecate ((:)(start::T, stop::T) where {T <: ZonedDateTime}) start:Day(1):stop false
-end
-
-# END TimeZones 0.6 deprecations
 
 # BEGIN TimeZones 0.9 deprecations
 
