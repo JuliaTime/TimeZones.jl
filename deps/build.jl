c847a356773ab1720bc582a006f8e8d3fc900031
Curtis Vogt <curtis.vogt@gmail.com>
2019-09-27 20:18:36 -0500

    Default to using 2019b tzdata (#214)

--- deps/build.jl
+++ deps/build.jl
@@ -3,4 +3,4 @@ import TimeZones: build
 # ENV variable allows users to modify the default to be "latest". Do NOT use "latest"
 # as the default here as can make it difficult to debug to past versions of working code.
 # Note: Also allows us to only download a single tzdata version during CI jobs.
-build(get(ENV, "JULIA_TZ_VERSION", "2019a"))
+build(get(ENV, "JULIA_TZ_VERSION", "2019b"))
