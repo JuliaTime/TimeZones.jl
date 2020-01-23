76eb59c41ce898e60f4ea33308fdf0664d2ba9e1
Curtis Vogt <curtis.vogt@gmail.com>
2020-01-23 15:39:15 -0600

    Drop 0.11 deprecations

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,9 +1,4 @@
 using Base: @deprecate
 
 # BEGIN TimeZones 1.0 deprecations
-
-@deprecate localtime(zdt::ZonedDateTime) DateTime(zdt, Local) false
-@deprecate utc(zdt::ZonedDateTime) DateTime(zdt, UTC) false
-@deprecate DateTime(zdt::ZonedDateTime) DateTime(zdt, Local)
-
 # END TimeZones 1.0 deprecations
