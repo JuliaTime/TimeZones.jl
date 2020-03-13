e493ba42d6f12cd1e9d65b6f3058402a650e515c
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-03-13 11:37:56 +0000

    tweak comment

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,5 +1,7 @@
-# Note: recipes seem to need to use short-form functions
-@recipe f(::Type{T}, val::T) where {T<:ZonedDateTime} = (
-    zdt -> Dates.value(DateTime(zdt, UTC)),
-    dt -> string(DateTime(Dates.UTM(dt))),
-)
+# Note: type-recipes seem to need to use short-form functions
+@recipe function f(::Type{<:ZonedDateTime}, val::ZonedDateTime)
+    return (
+        zdt -> Dates.value(DateTime(zdt, UTC)),
+        dt -> string(DateTime(Dates.UTM(dt))),
+    )
+end
