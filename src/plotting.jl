f56af238862e4c7b332ce047398c60cc44223b66
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-03-25 11:59:47 +0000

    use UTC type recipe but append +00:00 to string labels

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,3 +1,4 @@
+#==
 @recipe function f(xs::AbstractVector{<:ZonedDateTime}, ys)
     # if no zoned date times nothing to do but return empty list
     isempty(xs) && return xs
@@ -8,3 +9,10 @@
     xguide := strip(get(plotattributes, :xguide, "") * " (timezone: $tz)")
     new_xs, ys
 end
+
+==#
+
+@recipe f(::Type{T}, val::T) where {T<:ZonedDateTime} = (
+    zdt ->  Dates.value(DateTime(zdt, UTC)),
+    dt -> string(DateTime(Dates.UTM(dt))) * "+00:00",  # make clear we are in UTC
+)
