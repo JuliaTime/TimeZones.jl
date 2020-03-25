1f74f85c5f6de7f9a4f6ca436f85060a2eaefb41
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-03-25 18:13:31 +0000

    add comment about what this does

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,3 +1,12 @@
+#==
+Plot ZonedDateTime, on x-axis.
+We convert it to DateTimes, in the local timezone,
+and we list that timezone in the x-axis label.
+
+This is just one of many options for how to display this info.
+See this PR: https://github.com/JuliaTime/TimeZones.jl/pull/251#issuecomment-603806198
+for details on the options and their tradeoffs.
+==#
 @recipe function f(xs::AbstractVector{<:ZonedDateTime}, ys)
     # if no zoned date times nothing to do but return empty list
     isempty(xs) && return xs
