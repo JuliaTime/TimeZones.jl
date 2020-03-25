9b7c9a0a9b073a42080cbe0674d646950009d611
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-03-25 11:59:47 +0000

    Revert "use UTC type recipe but append +00:00 to string labels"
    This reverts commit f56af238862e4c7b332ce047398c60cc44223b66.

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,4 +1,3 @@
-#==
 @recipe function f(xs::AbstractVector{<:ZonedDateTime}, ys)
     # if no zoned date times nothing to do but return empty list
     isempty(xs) && return xs
@@ -9,10 +8,3 @@
     xguide := strip(get(plotattributes, :xguide, "") * " (timezone: $tz)")
     new_xs, ys
 end
-
-==#
-
-@recipe f(::Type{T}, val::T) where {T<:ZonedDateTime} = (
-    zdt ->  Dates.value(DateTime(zdt, UTC)),
-    dt -> string(DateTime(Dates.UTM(dt))) * "+00:00",  # make clear we are in UTC
-)
--- test/plotting.jl
+++ test/plotting.jl
@@ -1,16 +1,20 @@
 @testset "ZonedDateTime plot recipe" begin
-    # This time period crosses a DST transition where clocks are set back 1 hour
-    start_zdt = ZonedDateTime(2017,10, 28, 12, 1, 1, tz"Europe/London")
-    end_zdt = ZonedDateTime(2017,10, 29, 12, 1, 1, tz"Europe/London")
+    start_zdt = ZonedDateTime(2017,1,1,0,0,0, tz"EST")
+    end_zdt = ZonedDateTime(2017,1,1,10,30,0, tz"EST")
     zoned_dates = start_zdt:Hour(1):end_zdt
 
-    @testset "Base case" begin
-        result = scatter(zoned_dates, 1:11)
-        
-        x_axis = result.subplots[1][:xaxis]
+    function check_extrema(x_axis)
         date_val(zdt) = Dates.value(DateTime(zdt, Local))
+
         @test x_axis[:extrema].emin ≈ date_val(start_zdt) rtol=0.000_000_1
         @test x_axis[:extrema].emax ≈ date_val(end_zdt) rtol=0.000_000_1
+    end
+
+    @testset "No label" begin
+        result = scatter(zoned_dates, 1:11)
+        x_axis = result.subplots[1][:xaxis]
+        check_extrema(x_axis)
+        @test x_axis[:guide] == "(timezone: EST)"
     end
 
     @testset "label" begin
