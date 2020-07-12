540384039fd98ccc64afda42609fd534901c6c17
Matěj Račinský <racinsky.matej@seznam.cz>
2020-07-12 13:37:26 +0200

    using artifacts for windows xml

--- src/winzone/WindowsTimeZoneIDs.jl
+++ src/winzone/WindowsTimeZoneIDs.jl
@@ -49,9 +49,15 @@ function build(xml_file::AbstractString=WINDOWS_XML_FILE; force::Bool=false)
         if isfile(fallback_xml_file) && !force
             cp(fallback_xml_file, xml_file)
         else
-            @info "Downloading latest Windows to POSIX timezone ID XML"
-            # todo: instead of this, add the whole repo as a tarball
-            download(WINDOWS_ZONE_URL, xml_file)
+            if VERSION >= v"1.4"
+                @info "Downloading Windows to POSIX timezone ID XML from unicode-org/cldr repo, version 37"
+                xml_dir = artifact"tzdata_windowsZones"
+                # no version specified in the repo so I could grep it and print it here
+                cp(joinpath(xml_dir, "cldr-release-37", "common", "supplemental", "windowsZones.xml"), xml_file)
+            else
+                @info "Downloading latest Windows to POSIX timezone ID XML"
+                download(WINDOWS_ZONE_URL, xml_file)
+            end
         end
     end
 
