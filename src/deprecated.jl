1fa1bc959a2c80fd96fedafe611d62de4fd89599
Curtis Vogt <curtis.vogt@gmail.com>
2020-09-28 13:11:48 -0500

    Replace DateTime(zdt, Local) with DateTime(zdt)

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,4 +1,9 @@
 using Base: @deprecate
 
 # BEGIN TimeZones 1.0 deprecations
+
+@deprecate Dates.DateTime(zdt::ZonedDateTime, ::Type{Local}) DateTime(zdt)
+@deprecate Dates.Date(zdt::ZonedDateTime, ::Type{Local}) Date(zdt)
+@deprecate Dates.Time(zdt::ZonedDateTime, ::Type{Local}) Time(zdt)
+
 # END TimeZones 1.0 deprecations
