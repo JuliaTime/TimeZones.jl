f8e838b91a0699ddfd0be8a1d7328946fdaef108
Eric Davies <iamed2@gmail.com>
2020-11-18 12:13:14 -0600

    Fix readarchive typo

--- src/tzdata/build.jl
+++ src/tzdata/build.jl
@@ -87,7 +87,7 @@ function build(
 
         if !isempty(tz_source_dir)
             @info "Extracting $version tzdata archive"
-            regions = union!(intersect(regions, readdir(artifact_dir)), CUSTOM_REGIONS)
+            regions = union!(intersect(regions, readarchive(archive)), CUSTOM_REGIONS)
             extract(archive, tz_source_dir, setdiff(regions, CUSTOM_REGIONS), verbose=verbose)
         end
     end
