ae25a0d5e89e9d53a1fdcb8ccc7c0afae87441fb
Lyndon White <oxinabox@ucc.asn.au>
2020-12-09 18:48:34 +0000

    use nested dir per julia version
    Co-authored-by: Curtis Vogt <curtis.vogt@gmail.com>

--- src/tzdata/TZData.jl
+++ src/tzdata/TZData.jl
@@ -16,7 +16,7 @@ end
 
 const ARCHIVE_DIR = joinpath(DEPS_DIR, "tzarchive")
 const TZ_SOURCE_DIR = joinpath(DEPS_DIR, "tzsource")
-const COMPILED_DIR = joinpath(DEPS_DIR, string("compiled_julia_v", VERSION))
+const COMPILED_DIR = joinpath(DEPS_DIR, "compiled", string(VERSION))
 
 const ARTIFACT_TOML = joinpath(@__DIR__, "..", "..", "Artifacts.toml")
 
