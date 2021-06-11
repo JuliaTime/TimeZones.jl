8340ee5721a9593d2084075cd94071eb0f1de9a9
Mike <michaelhatherly@gmail.com>
2021-06-11 14:28:26 +0100

    Correctly dereference `WINDOWS_TRANSLATION`.

--- test/winzone/WindowsTimeZoneIDs.jl
+++ test/winzone/WindowsTimeZoneIDs.jl
@@ -10,11 +10,11 @@ mktempdir() do temp_dir
     xml_file = joinpath(temp_dir, "windowZones.xml")
     @test !isfile(xml_file)
 
-    empty!(TimeZones.WindowsTimeZoneIDs.WINDOWS_TRANSLATION)
-    @test isempty(TimeZones.WindowsTimeZoneIDs.WINDOWS_TRANSLATION)
+    empty!(TimeZones.WindowsTimeZoneIDs.WINDOWS_TRANSLATION[])
+    @test isempty(TimeZones.WindowsTimeZoneIDs.WINDOWS_TRANSLATION[])
 
     # Does not perform download
     TimeZones.WindowsTimeZoneIDs.build(xml_file)
     @test isfile(xml_file)
-    @test !isempty(TimeZones.WindowsTimeZoneIDs.WINDOWS_TRANSLATION)
+    @test !isempty(TimeZones.WindowsTimeZoneIDs.WINDOWS_TRANSLATION[])
 end
