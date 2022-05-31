7c56bbf34dd01085fd67fa242f658a7078774f2e
Curtis Vogt <curtis.vogt@gmail.com>
2022-05-30 23:26:33 -0500

    Add deprecations

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -6,4 +6,9 @@ using Base: @deprecate
 @deprecate Date(zdt::ZonedDateTime, ::Type{Local}) Date(zdt)
 @deprecate Time(zdt::ZonedDateTime, ::Type{Local}) Time(zdt)
 
+const TZFILE_MAX = TZFile.TZFILE_CUTOFF
+const TransitionTimeInfo = TZFile.TransitionTimeInfo
+@deprecate abbreviation TZFile.abbreviation false
+@deprecate read_tzfile TZFile.read_tzfile false
+
 # END TimeZones 1.0 deprecations
