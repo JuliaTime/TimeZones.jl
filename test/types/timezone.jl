60635fb0e76cbcefc2872b8dd9b4c32dc2489bd6
Nick Robinson <npr251@gmail.com>
2022-12-14 14:42:02 +0000

    Test `TimeZone` has minimal allocations
    - Should not allocate for cached FixedTimeZone
    - Should allocate only once for cached VariableTimeZone

--- test/types/timezone.jl
+++ test/types/timezone.jl
@@ -1,5 +1,15 @@
 using TimeZones: Class
 
+@testset "TimeZone allocations" begin
+    tz = TimeZone("UTC")  # run once for compilation and to populate cache
+    @assert tz isa FixedTimeZone
+    @test 0 == @allocations(TimeZone("UTC"))
+
+    tz = TimeZone("America/Winnipeg")  # populate cache
+    @assert tz isa VariableTimeZone
+    @test 1 == @allocations(TimeZone("America/Winnipeg"))
+end
+
 @testset "istimezone" begin
     @test istimezone("Europe/Warsaw")
     @test istimezone("UTC+02")
