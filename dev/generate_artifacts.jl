e15cf6f7a9dc310433c952fefbc6e32e5511036a
Curtis Vogt <curtis.vogt@gmail.com>
2023-07-26 22:05:20 -0400

    Update generate_artifacts to use OS/arch specific unicode-cldr

--- dev/generate_artifacts.jl
+++ dev/generate_artifacts.jl
@@ -1,49 +1,100 @@
 using Base: SHA1
+using Base.BinaryPlatforms: HostPlatform, Platform
+using CodecZlib: GzipDecompressorStream
+using Downloads: download
 using JSON3: JSON3
 using HTTP: HTTP
-using Inflate: inflate_gzip
-using Pkg.Artifacts
-using SHA: sha256
+using Pkg.Artifacts: AbstractPlatform, artifact_hash, artifact_meta, bind_artifact!,
+    load_artifacts_toml, unbind_artifact!
+using SHA: SHA
 using Tar: Tar
 using TimeZones.TZData: tzdata_versions
 
 
+function artifact_checksums(tarball_url::AbstractString)
+    tarball = download(tarball_url, IOBuffer())
+
+    # Compute the Artifact.toml `sha256` from the compressed archive.
+    sha256 = bytes2hex(SHA.sha256(seekstart(tarball)))
+
+    # Compute the Artifact.toml `git-tree-sha1`. Usually this is computed via
+    # `Artifacts.create_artifact` but we want to avoid actually storing this data as an
+    # artifact.
+    git_tree_sha1 = SHA1(Tar.tree_hash(GzipDecompressorStream(seekstart(tarball))))
+
+    return git_tree_sha1, sha256
+end
+
 # Code loosely based upon: https://julialang.github.io/Pkg.jl/dev/artifacts/#Using-Artifacts-1
-function bind_artifact_url!(artifacts_toml::String, name::String, url::String; lazy::Bool=true)
+function bind_artifact_url!(
+    artifacts_toml::String,
+    name::String,
+    url::String;
+    lazy::Bool=true,
+    platform::Union{AbstractPlatform,Nothing}=nothing,
+)
+    p = something(platform, HostPlatform())
+
     # Query the `Artifacts.toml` file for the hash associated with artifact name. If no such
     # binding exists within the file then `nothing` will be returned.
-    artifact_hash = Artifacts.artifact_hash(name, artifacts_toml)
+    git_tree_sha1 = artifact_hash(name, artifacts_toml; platform=p)
 
-    if artifact_hash === nothing
+    if git_tree_sha1 === nothing
         @info "Processing new artifact: $name"
-
-        archive = download(url)
-        try
-            # Compute the `sha256` of the archive.
-            archive_sha = bytes2hex(open(sha256, archive))
-
-            # Compute the `git-tree-sha1`. Usually this is computed via
-            # `create_artifact` but we want to avoid actually storing this data as an
-            # artifact.
-            artifact_hash = SHA1(Tar.tree_hash(IOBuffer(inflate_gzip(archive))))
-        finally
-            rm(archive)
-        end
+        git_tree_sha1, sha256 = artifact_checksums(url)
     else
-        download_info = Artifacts.artifact_meta(name, artifacts_toml)["download"]
-        archive_sha = only(download_info)["sha256"]
+        meta = artifact_meta(name, artifacts_toml; platform=p)
+        sha256 = only(meta["download"])["sha256"]
     end
+
+    download_info = [(url, sha256)]
 
     # Must unbind before we can call bind on an already bound artifact
     unbind_artifact!(artifacts_toml, name)
-    bind_artifact!(
-        artifacts_toml,
-        name,
-        artifact_hash;
-        lazy,
-        download_info=[(url, archive_sha)],
-    )
+    bind_artifact!(artifacts_toml, name, git_tree_sha1; lazy, platform, download_info)
 end
+
+function update_uncode_cldr_artifacts!(artifacts_toml::AbstractString)
+    @info "Checking for latest Unicode CLDR release..."
+    response = HTTP.get("https://api.github.com/repos/unicode-org/cldr/releases/latest")
+    json = JSON3.read(response.body)
+    latest_unicode_cldr = json.tag_name  # latest release
+
+    @info "Latest Unicode CLDR release: $latest_unicode_cldr"
+    url = "https://github.com/unicode-org/cldr/archive/$latest_unicode_cldr.tar.gz"
+
+    artifact_dict = load_artifacts_toml(artifacts_toml)
+    unicode_artifacts = filter(startswith("unicode-cldr-"), keys(artifact_dict))
+
+    # Determine the checksum information from the artifacts or the download.
+    name = "unicode-cldr-$latest_unicode_cldr"
+    if name in unicode_artifacts
+        # Assumes that the same artifact is used across various platforms. That is true
+        # for our use of the unicode-cldr artifact.
+        info = first(artifact_dict[name])
+        git_tree_sha1 = SHA1(info["git-tree-sha1"])
+        sha256 = only(info["download"])["sha256"]
+    else
+        @info "Processing new artifact: $name"
+        git_tree_sha1, sha256 = artifact_checksums(url)
+    end
+
+    download_info = [(url, sha256)]
+    platforms = [
+        Platform("x86_64", "windows"),
+        Platform("i686", "windows"),
+    ]
+
+    # Clear out old unicode-cldr versions by unbinding all of the relevant artifacts and
+    # only binding the latest version.
+    unbind_artifact!.(artifacts_toml, unicode_artifacts)
+    for platform in platforms
+        bind_artifact!(artifacts_toml, name, git_tree_sha1; platform, download_info)
+    end
+
+    return latest_unicode_cldr
+end
+
 
 function update_artifacts!(artifacts_toml::String)
     @info "Checking for missing tzdata versions..."
@@ -57,14 +108,7 @@ function update_artifacts!(artifacts_toml::String)
         bind_artifact_url!(artifacts_toml, artifact_name, url; lazy)
     end
 
-    @info "Checking for latest Unicode CLDR release..."
-    response = HTTP.get("https://api.github.com/repos/unicode-org/cldr/releases/latest")
-    json = JSON3.read(response.body)
-    latest_unicode_cldr = json.tag_name  # latest release
-
-    @info "Latest Unicode CLDR release: $latest_unicode_cldr"
-    url = "https://github.com/unicode-org/cldr/archive/$latest_unicode_cldr.tar.gz"
-    bind_artifact_url!(artifacts_toml, "unicode-cldr-$latest_unicode_cldr", url)
+    latest_unicode_cldr = update_uncode_cldr_artifacts!(artifacts_toml)
 
     # Display the information on the latest releases which is useful for updating the
     # TimeZones package defaults.
