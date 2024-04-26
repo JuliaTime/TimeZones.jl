0556cb2528ec6b9321f93370f1257c40a36a49c8
Martijn Visser <mgvisser@gmail.com>
2024-04-26 10:17:08 +0200

    Apply suggestions from code review
    Co-authored-by: Curtis Vogt <curtis.vogt@gmail.com>

--- test/windows_zones.jl
+++ test/windows_zones.jl
@@ -1,3 +1,4 @@
-using TimeZones: WINDOWS_TRANSLATION
+using TimeZones: UNICODE_CLDR_VERSION, WINDOWS_TRANSLATION
 
+@test occursin(r"^release-\d+(-\d+)$", UNICODE_CLDR_VERSION)
 @test WINDOWS_TRANSLATION["Central European Standard Time"] == "Europe/Warsaw"
