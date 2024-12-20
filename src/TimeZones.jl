d6eff71b37aedd7f07204af4ddc4d5ceeb3bbfc8
Lorenzo Contento <16554887+lcontento@users.noreply.github.com>
2024-12-20 22:56:13 +0100

    Make `TimeZones.jl` relocatable (#479)
    * Determine tree hash directly from Artifacts.toml
    
    * Use `locate_package`
    
    * Test for sysimage
    
    ---------
    
    Co-authored-by: Curtis Vogt <curtis.vogt@gmail.com>

--- src/TimeZones.jl
+++ src/TimeZones.jl
@@ -1,5 +1,6 @@
 module TimeZones
 
+using Artifacts: Artifacts
 using Dates
 using Printf
 using Scratch: @get_scratch!
@@ -33,13 +34,27 @@ export TimeZone, @tz_str, istimezone, FixedTimeZone, VariableTimeZone, ZonedDate
 
 _scratch_dir() = @get_scratch!("build")
 
-const _COMPILED_DIR = Ref{String}(TZJData.ARTIFACT_DIR)
+const _COMPILED_DIR = Ref{String}()
 
 # TimeZone types used to disambiguate the context of a DateTime
 # abstract type UTC <: TimeZone end  # Already defined in the Dates stdlib
 abstract type Local <: TimeZone end
 
 function __init__()
+    # Set at runtime to ensure relocatability
+    _COMPILED_DIR[] = @static if isdefined(TZJData, :artifact_dir)
+        TZJData.artifact_dir()
+    else
+        # Backwards compatibility for TZJData versions below v1.3.1. The portion of the
+        # code which determines the `pkg_dir` could be replaced by `pkgdir(TZJData)` however
+        # the `pkgdir` function doesn't work well with relocated system images.
+        pkg = Base.identify_package(TZJData, "TZJData")
+        pkg_dir = dirname(dirname(Base.locate_package(pkg)))
+        artifact_dict = Artifacts.parse_toml(joinpath(pkg_dir, "Artifacts.toml"))
+        hash = Base.SHA1(artifact_dict["tzjdata"]["git-tree-sha1"])
+        Artifacts.artifact_path(hash)
+    end
+
     # Dates extension needs to happen everytime the module is loaded (issue #24)
     init_dates_extension()
 
