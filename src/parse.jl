b51e9034458259b941fca229a70bb274dc6862ff
Curtis Vogt <curtis.vogt@gmail.com>
2025-02-03 14:16:21 -0600

    Documentation update

--- src/parse.jl
+++ src/parse.jl
@@ -26,10 +26,10 @@ end
 When the `TimeZones` package is loaded, 2 extra character codes are available
 for constructing the `format` string:
 
-| Code       | Matches   | Comment                                                       |
-|:-----------|:----------|:--------------------------------------------------------------|
-| `z`        | +02:00    | Numeric UTC offset                                            |
-| `Z`        | GST, UTC  | Time zone abbreviation                                        |
+| Code       | Matches            | Comment                                               |
+|:-----------|:-------------------|:------------------------------------------------------|
+| `z`        | +02:00, -0100, +14 | Parsing matches a fixed numeric UTC offset `±hh:mm`, `±hhmm`, or `±hh`. Formatting outputs `±hh:mm` |
+| `Z`        | UTC, GMT, America/New_York | Name of a time zone as specified in the IANA tz database |
 """ DateFormat
 
 function Base.parse(::Type{ZonedDateTime}, str::AbstractString)
--- src/types/zoneddatetime.jl
+++ src/types/zoneddatetime.jl
@@ -173,7 +173,29 @@ end
 
 Construct a `ZonedDateTime` by parsing `str`. This method is designed so that
 `zdt == ZonedDateTime(string(zdt))` where `zdt` can be any `ZonedDateTime`
-object.
+object. Take note that this method will always create a `ZonedDateTime` with a
+`FixedTimeZone` which can result in different results with date/time arithmetic.
+
+## Examples
+```jltest
+julia> zdt = ZonedDateTime(2025, 3, 8, 9, tz"America/New_York")
+2025-03-08T09:00:00-05:00
+
+julia> timezone(zdt)
+America/New_York (UTC-5/UTC-4)
+
+julia> zdt + Day(1)
+2025-03-09T09:00:00-04:00
+
+julia> pzdt = ZonedDateTime(string(zdt))
+2025-03-08T09:00:00-05:00
+
+julia> timezone(pzdt)
+UTC-05:00
+
+julia> pzdt + Day(1)
+2025-03-09T09:00:00-05:00
+```
 """
 ZonedDateTime(str::AbstractString) = parse(ZonedDateTime, str)
 
