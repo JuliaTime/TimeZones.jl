0a6ade50ecd3d0d64d760cf6888cd368e4e5def5
Curtis Vogt <curtis.vogt@gmail.com>
2025-11-26 15:26:20 -0600

    Wrap up

--- src/tzdata/TZData.jl
+++ src/tzdata/TZData.jl
@@ -12,11 +12,6 @@ using p7zip_jll: p7zip_jll
 
 export REGIONS, LEGACY_REGIONS
 
-const FOO = Ref{String}()
-function __init__()
-    FOO[] = _scratch_dir()
-end
-
 include("timeoffset.jl")
 include("version.jl")
 include("archive.jl")
